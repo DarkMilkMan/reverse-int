e3cf45881944bec05ee62b30874ae7f09896d283
Dmitry <darkmilkman@gmail.com>
2023-02-27 16:43:41 +0300

    feat: success

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  if (n<0){
+    n = Math.abs(n)
+  }
+  n = String(n);
+  for (let i=n.length-1;i>=0;i--){
+    result += n[i];
+  }
+  return result;
 }
